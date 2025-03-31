5ded4f836bee9977aed10df6e80da63107eb888a
hawke <tom.klucar@gmail.com>
2025-03-31 11:44:53 +0200

    texttype and textnode classes

--- src/main.py
+++ src/main.py
@@ -1,1 +1,11 @@
-print("Hello world")
+from textnode import TextNode, TextType
+
+def main():
+    dummy_text_node_1 = TextNode("Hello", TextType.TEXT, "https://example.com")
+    dummy_text_node_2 = TextNode("World", TextType.BOLD)
+
+    print(dummy_text_node_1.__repr__())
+    print(dummy_text_node_2.__repr__())
+
+
+main()
---
+++ src/textnode.py
@@ -0,0 +1,30 @@
+from enum import Enum
+
+class TextType(Enum):
+    TEXT = "TEXT"
+    BOLD = "BOLD"
+    ITALIC = "ITALIC"
+    CODE = "CODE"
+    LINK = "LINK"
+    IMAGE = "IMAGE"
+
+class TextNode:
+    def __init__(self, text, text_type, url=None):
+        if not isinstance(text_type, TextType):
+            raise TypeError("text_type must be an instance of TextType Enum")
+        
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other):
+        if not isinstance(other, TextNode):
+            return False
+        
+        return (self.text == other.text and
+                self.text_type == other.text_type and
+                self.url == other.url)
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+    
