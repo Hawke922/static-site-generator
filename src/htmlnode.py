94902269370d2bcc3c2fade91736f4f7a61639ca
hawke <tom.klucar@gmail.com>
2025-03-31 14:41:16 +0200

    leafnode inside html node class files

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -21,3 +21,32 @@ class HTMLNode:
 
     def __repr__(self):
         return f"HTMLNode(tag={self.tag}, value={self.value}, children={self.children}, props={self.props})"
+    
+    
+class LeafNode(HTMLNode):
+    def __init__(self, tag=None, value=None, props=None):
+        if value is None:
+            raise ValueError("LeafNode requires a value")
+        super().__init__(tag=tag, value=value, props=props)
+
+    def to_html(self):
+        if self.tag is None:
+            return self.value
+        
+        props_str = self.props_to_html()
+
+        if props_str:
+            return f"<{self.tag} {props_str}>{self.value}</{self.tag}>"
+        else:
+            return f"<{self.tag}>{self.value}</{self.tag}>"
+        
+    def __eq__(self, other):
+        if not isinstance(other, LeafNode):
+            return False
+        return (self.tag == other.tag and
+                self.value == other.value and
+                self.children == other.children and
+                self.props == other.props)
+    
+    def __repr__(self):
+        return f"LeafNode(tag={self.tag}, value={self.value}, props={self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -25,6 +25,49 @@ class TestHTMLNode(unittest.TestCase):
         node2 = HTMLNode("div", "This is a div", {"class": "container"})
         self.assertNotEqual(node, node2)
 
+class TestLeafNode(unittest.TestCase):
+    def test_leafnode_requires_value(self):
+        with self.assertRaises(ValueError) as context:
+            LeafNode(tag="p")
+        self.assertEqual(str(context.exception), "LeafNode requires a value")
+
+    def test_leafnode_initialization(self):
+        node = LeafNode(tag="p", value="Hello, world!")
+        self.assertEqual(node.tag, "p")
+        self.assertEqual(node.value, "Hello, world!")
+        self.assertEqual(node.children, [])
+        self.assertEqual(node.props, {})
+
+    def test_leafnode_to_html_without_props(self):
+        node = LeafNode(tag="p", value="Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leafnode_to_html_with_props(self):
+        node = LeafNode(tag="p", value="Hello, world!", props={"class": "text-bold", "id": "greeting"})
+        self.assertEqual(node.to_html(), '<p class="text-bold" id="greeting">Hello, world!</p>')
+
+    def test_leafnode_to_html_without_tag(self):
+        node = LeafNode(value="Just text")
+        self.assertEqual(node.to_html(), "Just text")
+
+    def test_leafnode_equality(self):
+        node1 = LeafNode(tag="p", value="Hello", props={"class": "text-bold"})
+        node2 = LeafNode(tag="p", value="Hello", props={"class": "text-bold"})
+        self.assertEqual(node1, node2)
+
+    def test_leafnode_inequality_different_value(self):
+        node1 = LeafNode(tag="p", value="Hello")
+        node2 = LeafNode(tag="p", value="World")
+        self.assertNotEqual(node1, node2)
+
+    def test_leafnode_inequality_different_props(self):
+        node1 = LeafNode(tag="p", value="Hello", props={"class": "text-bold"})
+        node2 = LeafNode(tag="p", value="Hello", props={"id": "greeting"})
+        self.assertNotEqual(node1, node2)
+
+    def test_leafnode_repr(self):
+        node = LeafNode(tag="p", value="Hello", props={"class": "text-bold"})
+        self.assertEqual(repr(node), "LeafNode(tag=p, value=Hello, props={'class': 'text-bold'})")
 
 if __name__ == "__main__":
     unittest.main()
